c70f648e6f3a27f781e93d0f509b1a5a1b52bbca
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-06-10 21:44:28 +0200

    HomeComponent.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-    
+      <Router>
+        <Navbar />
+        <Routes>
+          
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import GithubIcon from "@material-ui/icons/GitHub";
+import "../styles/Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="about">
+        <h2>Hi, My name is Milos Mijajlovic</h2>
+        <div className="prompt">
+          <p>Junior Developer with a passion for learning and creating</p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GithubIcon />
+        </div>
+          </div>
+          <div className="skills">
+              <h1>Skills</h1>
+              <ol className="list">
+                  <li className="item">
+                      <h2>Front-end</h2>
+                      <span>HTML5(CANVAS), JAVASCRIPT, PIXI.JS, REACT, CASS, SASS, NPM, BOOTSTRAP, MATERIALUI, YARN, STYLEDCOMPONENTS</span>
+                  </li>
+                  <li className="item">
+                      <h2>Back-end</h2>
+                      <span>NODEJS, MYSQL, PHP, LARAVEL, CAKEPHP</span>
+                  </li>
+                  
+              </ol>
+          </div>
+    </div>
+  );
+}
