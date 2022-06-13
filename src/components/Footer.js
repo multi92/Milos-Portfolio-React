9d438a5b7f4390cd6716ee48999de0e593d94d17
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-06-13 19:58:57 +0200

    add IconButton to FooterComponent

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,41 @@ import InstagramIcon from "@material-ui/icons/Instagram";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import { Icon, IconButton } from "@material-ui/core";
 import "../styles/Footer.css";
 
 export default function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon />
-        <TwitterIcon />
-        <FacebookIcon />
-        <LinkedInIcon />
-          </div>
-          <p> &copy; 2022 Milos Mijajlovic</p>
+        <IconButton
+          onClick={() =>
+            window.open("https://www.podinario.rs/")
+          }
+        >
+          <InstagramIcon />
+        </IconButton>
+        <IconButton
+          onClick={() =>
+            window.open(
+              "https://www.podinario.rs/"
+            )
+          }
+        >
+          <FacebookIcon />
+        </IconButton>
+
+        <IconButton>
+          <LinkedInIcon
+            onClick={() =>
+              window.open(
+                "https://www.podinario.rs/"
+              )
+            }
+          />
+        </IconButton>
+      </div>
+      <p> &copy; 2022 Milos Mijajlovic</p>
     </div>
   );
 }
