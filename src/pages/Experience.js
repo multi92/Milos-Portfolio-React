8d27402b6760f35dfb3413daf4b39a66402b081d
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-06-14 20:48:54 +0200

    Experience Component

---
+++ src/pages/Experience.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+    VerticalTimeline,
+    VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@material-ui/icons/School";
+import WorkIcon from "@material-ui/icons/Work";
+
+
+
+function Experiance() {
+  return (
+    <div className="expiriance">Experiance</div>
+  )
+}
+
+export default Experiance
